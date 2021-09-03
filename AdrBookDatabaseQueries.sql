53c83664e2458afc20a2ee3220d0dbafaa666c8d
omkargb <omkargb107@gmail.com>
2021-09-03 20:01:57 +0530

    [omkargb] Add : Contact table created in address book database.

--- AdrBookDatabaseQueries.sql
+++ AdrBookDatabaseQueries.sql
@@ -1,3 +1,15 @@
 create database AddressBookServiceDB
 
 use AddressBookServiceDB
+
+create table ContactsTable(
+Id int identity(1,1) NOT NULL PRIMARY KEY,
+FirstName varchar(16) ,
+LastName varchar(16),
+Address varchar(24),
+City varchar(16),
+State varchar(16),
+ZipCode int,
+PhoneNumber varchar(16),
+EmailId varchar(24)
+);
